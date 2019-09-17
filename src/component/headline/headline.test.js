305edc875f3299189b8aa8265f1590d503cbeeaa
Vytautas Vasiliauskas <vytautas.eilika@gmail.com>
2019-09-17 16:34:46 +0300

    test prop types

--- src/component/headline/headline.test.js
+++ src/component/headline/headline.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import checkPropTypes from 'check-prop-types'
+
 import Headline from './index';
 
 import { findByTestAttr } from './../../Utils';
@@ -10,6 +12,29 @@ const setUp = (props={}) => {
 };
 
 describe('Headline Component', () => {
+
+    describe('Checking PropTypes', () => {
+
+        it('Should not throw a warning', () => {
+
+            const expectedProps = {
+                header: 'Test Header',
+                desc: 'Test Desc',
+                tempArr: [{
+                    fName: 'Test fName',
+                    lName: 'Test lName',
+                    email: 'test@email.com',
+                    age: 23,
+                    onlineStatus: false
+                }]
+            };
+                                                                                
+            const propsErr = checkPropTypes(Headline.propTypes, expectedProps, 'props', Headline.name);
+            expect(propsErr).toBeUndefined();
+
+        });
+
+    });
 
     describe('Have props', () => {
 
--- src/component/headline/index.js
+++ src/component/headline/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Headline extends Component {
 
@@ -24,5 +25,19 @@ class Headline extends Component {
         )
     }
 }
+
+Headline.propTypes = {
+    header: PropTypes.string,
+    desc: PropTypes.string,
+    // Complex example for working with PropTypes (not used in component)
+    tempArr: PropTypes.arrayOf(PropTypes.shape({
+        fName: PropTypes.string,
+        lName: PropTypes.string,
+        email: PropTypes.string,
+        age: PropTypes.number,
+        onlineStatus: PropTypes.bool
+    }))
+}
+
 
 export default Headline; 
