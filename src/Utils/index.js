a0d93b9c5102040613799c99e1f25ee7ad7c1392
Vytautas Vasiliauskas <vytautas.eilika@gmail.com>
2019-09-18 11:59:28 +0300

    connected component

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -1,5 +1,9 @@
 import checkPropTypes from 'check-prop-types';
 
+import { applyMiddleware, createStore } from 'redux';
+import rootReducer from './../reducers/index';
+import { middlewares } from './../createStore';
+
 export const findByTestAttr = ( component, attr ) => {
     return component.find(`[data-test="${attr}"]`)
 }
@@ -7,4 +11,9 @@ export const findByTestAttr = ( component, attr ) => {
 export const checkProps = (component, expectedProps) => {
     const propsErr = checkPropTypes(component.propTypes, expectedProps, 'props', component.name);
     return propsErr;
+}; 
+
+export const testStore = (initialState) => {
+    const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+    return createStoreWithMiddleware(rootReducer, initialState);
 }; 
